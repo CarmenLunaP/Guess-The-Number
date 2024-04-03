140ff17bef5ebe67b914979f0652193a7dd8e8fe
Carmen Luna <lunacarmen22@gmail.com>
2024-04-03 17:11:06 -0500

    Welcome the gamer

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+     Console.WriteLine("Please enter your name to begin the game:?"); 
+     var name = Console.ReadLine();
+     Console.WriteLine($"{Environment.NewLine}Hello {name}, Welcome to Guess the Number game");
+
+
+
     }
 }
