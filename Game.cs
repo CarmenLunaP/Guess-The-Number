6ccb3b4928803ca7b7be8b245701046798d0ee3e
Carmen Luna <lunacarmen22@gmail.com>
2024-04-04 11:04:55 -0500

    Finish Hito 2

---
+++ Game.cs
@@ -0,0 +1,81 @@
+namespace Guess_The_Number;
+
+class Game
+{
+    public void PlayGame()
+    {
+
+        Random random = new Random();
+        // declaro un bolleano para reinicar el juego al final.
+        bool playAgain = true;
+        // declaro el bucle que es el cuerpo de juego cada vez que el usuario quiera continuar jugando
+        while (playAgain)
+        {
+            int guess = 0; // el numero que el usuario adiviva
+            int guesses = 0; // numeros de intentos
+            int number = random.Next(1, 101); //genero el aleatorio en la variable number
+
+            // declaro el bucle para inciar la partida
+            while (guess != number)
+            {
+
+                // Solicitar al usuario que ingrese su suposición
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Can you guess the secret number? Enter a number between 1 and 100:");
+                Console.ResetColor();
+
+                // Lee la entrada del usuario y convertirla a un número entero
+                guess = Convert.ToInt32(Console.ReadLine());
+                // Incremento el contador de intentos
+                guesses++;
+                // agrego las condiciones de acuerdo a ellas doy pistas al usuario
+                if (guess > number)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(guess + " is too high!");
+                    Console.WriteLine("Try a lower number.");
+                    Console.ResetColor();
+                }
+                else if (guess < number)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(guess + " is too low!");
+                    Console.WriteLine("Try a higher number.");
+                    Console.ResetColor();
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(guess + " is Correct! You guessed it in " + guesses + " attempts.");
+            Console.ResetColor();
+
+            // Pregunto al usuario si desea jugar nuevamente
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Would you like to play again? (y/n)");
+            Console.ResetColor();
+            //leo la respuesta
+            string playAgainInput = Console.ReadLine();
+            // activo el play again
+            playAgain = (playAgainInput.ToLower() == "y");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
-﻿namespace Guess_The_Number;
+﻿
+namespace Guess_The_Number;
 
 class Program
 {
     static void Main(string[] args)
     {
-     Console.WriteLine("Please enter your name to begin the game:?"); 
-     var name = Console.ReadLine();
-     Console.WriteLine($"{Environment.NewLine}Hello {name}, Welcome to Guess the Number game");
+        Console.ForegroundColor = ConsoleColor.Cyan; // asigno el color
+        Console.WriteLine("Welcome to the Guess the Number game! In this game, you'll have to guess a secret number between 1 and 100.");
+        // Console.ResetColor(); // quito el color
 
+        // Console.ForegroundColor = ConsoleColor.Cyan; // asigno el color
+        Console.WriteLine("Please enter your name to begin the game:");
+        // Console.ResetColor(); // quito el color
 
+        var name = Console.ReadLine();
+        Console.WriteLine($"{Environment.NewLine}Hello {name}, Welcome to Guess the Number game");
+        Console.ResetColor(); // quito el color
+        // Crear una instancia de Game y llamar al método PlayGame
+        Game game = new Game();
+        game.PlayGame();
 
     }
 }
