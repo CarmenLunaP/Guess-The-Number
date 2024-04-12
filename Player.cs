48fdcc07eb34257cc5f134373e9b75fe66445cae
Carmen Luna <lunacarmen22@gmail.com>
2024-04-12 09:48:29 -0500

    finisched hito 4

--- Player.cs
+++ Player.cs
@@ -1,30 +1,35 @@
 using System;
 
 namespace Guess_The_Number
-{ 
+{
     public class Player
     {
         public string Name { get; private set; }
-        public int LastGuess { get; private set; }
+        private int lastGuess;
 
         public Player(string name)
         {
             Name = name;
-            LastGuess = 0;
+            lastGuess = 0;
         }
 
         public void MakeGuess()
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Write("Can you guess the secret number? Enter a number between 1 and 100: ");
+            Console.Write("Enter a number between 1 and 100:");
             Console.ResetColor();
-            LastGuess = Convert.ToInt32(Console.ReadLine());
+            lastGuess = Convert.ToInt32(Console.ReadLine());
         }
 
-        // Método para reiniciar el último intento del jugador
+        public int GetLastGuess()
+        {
+            return lastGuess;
+        }
+
         public void ResetLastGuess()
         {
-            LastGuess = 0;
+            lastGuess = 0;
         }
     }
 }
+
